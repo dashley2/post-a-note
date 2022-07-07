7bdc757cee34bb8371d895c07dd305377663bdaa
Your Name <you@example.com>
2022-07-06 21:12:00 -0400

    Add route that front-end ute for front-end to request note data from

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
-const { notes } = require =('.db/db.json');
+const [ notes ] = require ('./Develop/db/db.json');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get('/api/notes'), (req, res) => {
+function filterByQuery(query, data) {
+    let filteredResults = data;
+    if (query.title) {
+      filteredResults = filteredResults.filter(note => note.title === query.title);
+    }
+    if (query.text) {
+      filteredResults = filteredResults.filter(note => note.text === query.text);
+    }
+    return filteredResults;
+  }
+
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    if (req.query) {
+      results = filterByQuery(req.query, results);
+    }
     res.json(notes);
-}
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
